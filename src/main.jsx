750c65dc97bf959a1c180a66f8ed7e34a1569bac
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-04 22:12:27 +0100

    Add the provider store to the main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store";
 import "./index.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <Router>
-    <App />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
