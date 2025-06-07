6ee76764fee6c01ef290816e90bd826ea47f0f62
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-06-07 22:04:29 +0100

    Create the artistApplication slice

---
+++ src/store/Slices/artistApplicationSlice.js
@@ -0,0 +1,90 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  artistApplication: null,
+  loading: false,
+  error: null,
+};
+
+const artistApplicationSlice = createSlice({
+  name: "artistApplication",
+  initialState,
+  reducers: {
+    // Fetch Artist Application
+    fetchArtistApplicationStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    fetchArtistApplicationSuccess(state, action) {
+      state.artistApplication = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    fetchArtistApplicationFailure(state, action) {
+      state.loading = false;
+      state.error = action.payload.error;
+    },
+
+    // Create Artist Application
+    createArtistApplicationStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    createArtistApplicationSuccess(state, action) {
+      state.artistApplication = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    createArtistApplicationFailure(state, action) {
+      state.loading = false;
+      state.error = action.payload.error;
+    },
+
+    // Update Artist Application
+    updateArtistApplicationStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    updateArtistApplicationSuccess(state, action) {
+      state.artistApplication = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateArtistApplicationFailure(state, action) {
+      state.loading = false;
+      state.error = action.payload.error;
+    },
+
+    // Delete Artist Application
+    deleteArtistApplicationStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    deleteArtistApplicationSuccess(state) {
+      state.artistApplication = null;
+      state.loading = false;
+      state.error = null;
+    },
+    deleteArtistApplicationFailure(state, action) {
+      state.loading = false;
+      state.error = action.payload.error;
+    },
+  },
+});
+
+export const {
+  fetchArtistApplicationStart,
+  fetchArtistApplicationSuccess,
+  fetchArtistApplicationFailure,
+  createArtistApplicationStart,
+  createArtistApplicationSuccess,
+  createArtistApplicationFailure,
+  updateArtistApplicationStart,
+  updateArtistApplicationSuccess,
+  updateArtistApplicationFailure,
+  deleteArtistApplicationStart,
+  deleteArtistApplicationSuccess,
+  deleteArtistApplicationFailure,
+} = artistApplicationSlice.actions;
+
+export default artistApplicationSlice.reducer;
