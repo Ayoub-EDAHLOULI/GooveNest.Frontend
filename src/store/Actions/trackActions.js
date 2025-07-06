9f15245134a26770e6294f53aec707ab76a8b8bd
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-07-06 20:10:59 +0100

    Create the fetch track by id method action

--- src/store/Actions/trackActions.js
+++ src/store/Actions/trackActions.js
@@ -2,9 +2,6 @@ import {
   fetchAllTracksStart,
   fetchAllTracksSuccess,
   fetchAllTracksFailure,
-  fetchPaginatedTracksStart,
-  fetchPaginatedTracksSuccess,
-  fetchPaginatedTracksFailure,
   fetchTrackByIdStart,
   fetchTrackByIdSuccess,
   fetchTrackByIdFailure,
@@ -49,6 +46,40 @@ export const fetchAllGenres = () => async (dispatch) => {
     dispatch(
       fetchAllTracksFailure({
         error: error.message || "An error occurred while fetching tracks",
+      })
+    );
+  }
+};
+
+export const fetchTrackById = (trackId) => async (dispatch) => {
+  dispatch(fetchTrackByIdStart());
+  try {
+    const response = await fetch(`${API_BASE_URL}track/${trackId}`, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      dispatch(
+        fetchTrackByIdFailure({
+          error: errorData.message || "Failed to fetch track.",
+        })
+      );
+      throw new Error(
+        errorData.message || "An error occurred while fetching track"
+      );
+    }
+
+    const data = await response.json();
+    dispatch(fetchTrackByIdSuccess(data.data));
+  } catch (error) {
+    dispatch(
+      fetchTrackByIdFailure({
+        error: error.message || "An error occurred while fetching track",
       })
     );
   }
