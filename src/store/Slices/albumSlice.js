f41c16604a5a2ec339c2b9ceed698ba077a01855
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-07-16 22:56:23 +0100

    Create the album slices

---
+++ src/store/Slices/albumSlice.js
@@ -0,0 +1,93 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const albumSlice = createSlice({
+  name: "album",
+  initialState: {
+    albums: [],
+    loading: false,
+    error: null,
+  },
+  reducers: {
+    fetchAlbumsStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    fetchAlbumsSuccess(state, action) {
+      state.loading = false;
+      state.albums = action.payload;
+    },
+    fetchAlbumsFailure(state, action) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+
+    // Create Album
+    createAlbumStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    createAlbumSuccess(state, action) {
+      state.loading = false;
+      state.albums.push(action.payload);
+    },
+    createAlbumFailure(state, action) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+
+    // Update Album
+    updateAlbumStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    updateAlbumSuccess(state, action) {
+      state.loading = false;
+      const index = state.albums.findIndex(
+        (album) => album.id === action.payload.id
+      );
+      if (index !== -1) {
+        state.albums[index] = action.payload;
+      }
+    },
+    updateAlbumFailure(state, action) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+
+    // Delete Album
+    deleteAlbumStart(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    deleteAlbumSuccess(state, action) {
+      state.loading = false;
+      state.albums = state.albums.filter(
+        (album) => album.id !== action.payload.id
+      );
+    },
+    deleteAlbumFailure(state, action) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
+});
+
+export const {
+  fetchAlbumsStart,
+  fetchAlbumsSuccess,
+  fetchAlbumsFailure,
+
+  createAlbumStart,
+  createAlbumSuccess,
+  createAlbumFailure,
+
+  updateAlbumStart,
+  updateAlbumSuccess,
+  updateAlbumFailure,
+
+  deleteAlbumStart,
+  deleteAlbumSuccess,
+  deleteAlbumFailure,
+} = albumSlice.actions;
+
+export default albumSlice.reducer;
