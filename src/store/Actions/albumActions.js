410d70cf540cca4185a9802bc148bded665400b4
Ayoub EDAHLOULI <ayoub.edahlouli@gmail.com>
2025-07-16 23:01:29 +0100

    Create the fetch all albums method action

---
+++ src/store/Actions/albumActions.js
@@ -0,0 +1,50 @@
+import {
+  fetchAlbumsStart,
+  fetchAlbumsSuccess,
+  fetchAlbumsFailure,
+  createAlbumStart,
+  createAlbumSuccess,
+  createAlbumFailure,
+  updateAlbumStart,
+  updateAlbumSuccess,
+  updateAlbumFailure,
+  deleteAlbumStart,
+  deleteAlbumSuccess,
+  deleteAlbumFailure,
+} from "../Slices/albumSlice";
+import { API_BASE_URL } from "../../config";
+
+// Fetch all albums
+export const fetchAllAlbums = () => async (dispatch) => {
+  dispatch(fetchAlbumsStart());
+  try {
+    const response = await fetch(`${API_BASE_URL}albums/all`, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      dispatch(
+        fetchAlbumsFailure({
+          error: errorData.message || "Failed to fetch albums.",
+        })
+      );
+      throw new Error(
+        errorData.message || "An error occurred while fetching albums"
+      );
+    }
+
+    const data = await response.json();
+    dispatch(fetchAlbumsSuccess(data.data));
+  } catch (error) {
+    dispatch(
+      fetchAlbumsFailure({
+        error: error.message || "An error occurred while fetching albums",
+      })
+    );
+  }
+};
